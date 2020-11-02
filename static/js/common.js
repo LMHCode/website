f5c039325a94f3602e8a4417d662143c4c0c361d
liuminghui <liuminghui@haiwang.link>
2020-11-02 18:51:08 +0800

    官网静态页

--- static/js/common.js
+++ static/js/common.js
@@ -3,4 +3,34 @@
   window.onbeforeunload = function (e) {
     window.scrollTo(0, 0)      
   }; 
-})
+
+  // 回到顶部监听
+  window.addEventListener('scroll', function (event) {
+    var scrolltopTemp = document.documentElement.scrollTop || document.body.scrollTop
+    var windowHeight = document.documentElement.clientHeight
+    if (document.documentElement.clientHeight < scrolltopTemp) {
+      $('.to-top').css({'display': 'flex'})
+    } else {
+      $('.to-top').css({'display': 'none'})
+    }
+  })
+
+  // 返回顶部
+  $('.to-top').click(function(e) {
+    window.scrollTo(0, 0)
+  })
+
+   // phone 
+   $('.show-phone').hover(function() {
+    $('.service-tell').addClass('service-tell-animation')
+  }, function() {
+    $('.service-tell').removeClass('service-tell-animation')
+  })
+  
+  // code
+  $('.show-code').hover(function() {
+    $('.fixed-code').addClass('fixed-code-animation')
+  }, function() {
+    $('.fixed-code').removeClass('fixed-code-animation')
+  })
+})()
