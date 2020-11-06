fda876d7f68599448abb61e8aefe5a7aba99af59
liuminghui <liuminghui@haiwang.link>
2020-11-06 17:20:33 +0800

    官网

--- static/js/common.js
+++ static/js/common.js
@@ -1,4 +1,35 @@
 (function() {
+  // phone
+  // 首页菜单点击
+  $('.phone-menu-btn').click(function() {
+    $('.phone-menu-btn').hide()
+    $('.phone-menu-close').show()
+    $('.phone-menu-tabs').show()
+  })
+  
+  $('.phone-menu-close').click(function() {
+    $('.phone-menu-close').hide()
+    $('.phone-menu-btn').show()
+    $('.phone-menu-tabs').hide()
+  })
+
+  // 手机端菜单按钮事件
+  $('.phone-tabs-item').click(function() {
+    $(this).addClass('phone-tab-active')
+    $(this).siblings('.phone-tabs-item').removeClass('phone-tab-active')
+    var states = $(this).attr('name')
+    if (states === 'home') {
+      // 主页
+    } else if (states === 'pro') {
+      // 产品介绍
+    } else if (states === 'store') {
+      // 药店联盟
+    } else if (states === 'aboutMe') {
+      // 关于我们
+    }
+  })
+  
+  //pc
   // 页面刷新 滚动到顶部
   window.onbeforeunload = function (e) {
     window.scrollTo(0, 0)      
@@ -26,7 +57,7 @@
   window.addEventListener('scroll', function (event) {
     var scrolltopTemp = document.documentElement.scrollTop || document.body.scrollTop
     var windowHeight = document.documentElement.clientHeight
-    console.log(windowHeight, scrolltopTemp)
+    // console.log(windowHeight, scrolltopTemp)
     if (windowHeight < scrolltopTemp) {
       $('.to-top').css({'display': 'flex'})
     } else {
